0f1f06f413a8566e8df5086ccc1c06d65c22f619
Sam Martinez <sam.martinez.dev@gmail.com>
2018-09-01 11:17:20 -0500

    Adds article controller/spec

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArticleController } from './article/article.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ArticleController],
   providers: [AppService],
 })
 export class AppModule {}
