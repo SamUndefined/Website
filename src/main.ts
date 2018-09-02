e2fc76e1e37913e1fd1c0b293728afede5c46f8d
SamUndefined <sam.undefined.dev@gmail.com>
2018-09-02 12:57:30 -0500

    Configure port to lsiten on from .env

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const port = process.env.port
+  const port = process.env.PORT
   const app = await NestFactory.create(AppModule)
   // tslint:disable-next-line:no-console
-  await app.listen(port).then(() => console.log('Listening on port ', port.toString()))
+  await app.listen(port)
+    .then(() => console.log('Listening on port ', port.toString()))
+    .catch(err => console.error(err))
 }
 bootstrap()
